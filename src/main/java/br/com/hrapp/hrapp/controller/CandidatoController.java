fbc7a563831b5d1941afac49c2c0f550dbec47c4
deborapaiva <debora.rubim.paiva@gmail.com>
2024-11-18 20:44:27 -0300

    Configuration Swagger - add Operation

--- src/main/java/br/com/hrapp/hrapp/controller/CandidatoController.java
+++ src/main/java/br/com/hrapp/hrapp/controller/CandidatoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import br.com.hrapp.hrapp.models.Vaga;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,18 +23,33 @@ public class CandidatoController {
 
     //CADASTRAR
     @PostMapping("/cadastrar")
+    @Operation(
+            summary = ("Cadastrar candidatos"),
+            description = ("Cadastro de candidatos"),
+            tags={"candidato"}
+    )
     public Candidato cadastrarCandidato(@RequestBody Candidato candidato) {
         return candidatoService.cadastrarCandidato(candidato);
     }
 
     // BUSCAR CANDIDATO POR CPF
     @GetMapping("/cpf/{cpf}")
+    @Operation(
+            summary = ("Buscar candidatos por cpf"),
+            description = ("Buscar por candidatos por cpf"),
+            tags={"candidato"}
+    )
     public Candidato buscarCandidatoPorCPF(@PathVariable String cpf) {
         return candidatoService.buscarCandidatoPorCPF(cpf);
     }
     
     //BUSCAR TODOS CANDIDATOS
     @GetMapping("/todos")
+    @Operation(
+            summary = ("Buscar todos candidato"),
+            description = ("Buscar por todos candidatos cadastrados"),
+            tags={"candidato"}
+    )
     public List<Candidato> buscarTodosCandidatos() {
         return candidatoService.buscarTodosCandidatos();
     }
@@ -46,6 +62,11 @@ public class CandidatoController {
 
     // DELETAR POR CPF
     @DeleteMapping("/delete/{cpf}")
+    @Operation(
+            summary = ("Deletar candidato"),
+            description = ("Deletar Vaga por CPF"),
+            tags={"candidato"}
+    )
     public ResponseEntity<Void> deletarCandidato(@PathVariable String cpf) {
         candidatoService.deletarCandidato(cpf);
         return ResponseEntity.noContent().build();
--- src/main/java/br/com/hrapp/hrapp/controller/VagaController.java
+++ src/main/java/br/com/hrapp/hrapp/controller/VagaController.java
@@ -2,6 +2,8 @@ package br.com.hrapp.hrapp.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import br.com.hrapp.hrapp.service.VagaService;
 
 @RestController
 @RequestMapping("/vaga")
+@Tag(name = "Vaga", description ="Controller para Vagas")
 public class VagaController {
 
     @Autowired
@@ -18,36 +21,66 @@ public class VagaController {
 
     // CADASTRAR VAGA 
     @PostMapping("/cadastrar")
+    @Operation(
+            summary = ("Cadastro de Vagas"),
+            description = ("Cadastro das vagas disponíveis pelo RH"),
+            tags={"vaga"}
+    )
     public Vaga cadastraVaga(@RequestBody Vaga vaga) {
         return vagaService.cadastraVaga(vaga);
     }
 
     // BUSCAR VAGA POR TÍTULO
     @GetMapping("/titulo/{titulo}")
+    @Operation(
+            summary = ("Busca de Vagas por Título"),
+            description = ("Busca de Vagas por Título"),
+            tags={"vaga"}
+    )
     public List<Vaga> buscarVagaPorTitulo(@PathVariable String titulo) {
         return vagaService.buscarVagaPorTitulo(titulo);
     }
 
     // BUSCAR VAGA POR STATUS
     @GetMapping("/status/{status}")
+    @Operation(
+            summary = ("Busca de Vagas por Status"),
+            description = ("Busca de Vagas por Status"),
+            tags={"vaga"}
+    )
     public List<Vaga> buscarVagaPorStatus(@PathVariable String status) {
         return vagaService.buscarVagaPorStatus(status);
     }
     
     //BUSCAR TODAS AS VAGAS
     @GetMapping("/todas")
+    @Operation(
+            summary = ("Busca de Todas as Vagas"),
+            description = ("Busca de Todas as Vagas"),
+            tags={"vaga"}
+    )
     public List<Vaga> buscarTodasVagas(){
     	return vagaService.buscarTodasVagas();
     }
 
     // ATUALIZAR VAGA
     @PutMapping("/{id}")
+    @Operation(
+            summary = ("Atualização de Vaga"),
+            description = ("Atualização de Vaga"),
+            tags={"vaga"}
+    )
     public Vaga atualizarVaga(@PathVariable Long id, @RequestBody Vaga vagaAtualizada) {
         return vagaService.atualizarVaga(id);
     }
 
     // EXCLUIR VAGA
     @DeleteMapping("/delete/{id}")
+    @Operation(
+            summary = ("Exclusão de Vaga"),
+            description = ("Excluir de Vaga por ID"),
+            tags={"vaga"}
+    )
     public ResponseEntity<Void> deletarVaga(@PathVariable Long id) {
         vagaService.deletarVaga(id);
         return ResponseEntity.noContent().build();
